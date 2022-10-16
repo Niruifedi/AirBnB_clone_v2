55a68656de287e776b02fcd4f12b9ad0db5f687c
Niruifedi <ifediniruizuchukwu@gmail.com>
2022-10-16 15:03:40 +0100

    fix

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -6,7 +6,7 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', strict_slashes=False)
 def hello():
     """
     Prints a message when / is called in the url
@@ -14,7 +14,7 @@ def hello():
     return 'Hello HBNB!'
 
 
-@app.route('/hbnb')
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """
     Prints a message when /hbnb is called in the url
@@ -22,7 +22,7 @@ def hbnb():
     return "HBNB"
 
 
-@app.route('/c/<text>')
+@app.route('/c/<text>', strict_slashes=False)
 def c_route(text):
     """
     Prints a message when /c/<text> is called in the url
@@ -30,8 +30,8 @@ def c_route(text):
     return 'C ' + text.replace('_', ' ')
 
 
-@app.route('/python/', defaults={'text': 'is_cool'})
-@app.route('/python/<text>')
+@app.route('/python/', defaults={'text': 'is_cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
 def py_route(text):
     """
     prints a message when /python/<text> is called in the url
@@ -41,4 +41,4 @@ def py_route(text):
 
 
 if __name__ == "__main__":
-    app.run(debug=True, strict_slashes=False)
+    app.run(debug=True)
